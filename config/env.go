4ccecf1ca6a4ddbf369b52da5aade2bb591c5f2c
Michael Quigley <michael@quigley.com>
2019-11-26 11:55:41 -0500

    Shared configuration components used by subsequent layers.

---
+++ config/env.go
@@ -0,0 +1,29 @@
+/*
+	Copyright 2019 Netfoundry, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package config
+
+import "os"
+
+func InjectEnv(config map[interface{}]interface{}) {
+	for key, v := range config {
+		if str, ok := v.(string); ok {
+			config[key] = os.ExpandEnv(str)
+		} else if m, ok := v.(map[interface{}]interface{}); ok {
+			InjectEnv(m)
+		}
+	}
+}
