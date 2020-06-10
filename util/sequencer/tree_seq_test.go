fe1c33f74dd97d7033f80bad46828822acc17bf0
Paul Lorenz <paul.lorenz@netfoundry.io>
2020-06-09 23:26:14 -0400

    Make close logic simpler, more idiomatic and hopefully more correct. Fixes GH-81

--- util/sequencer/tree_seq_test.go
+++ util/sequencer/tree_seq_test.go
@@ -46,7 +46,7 @@ func Test_treeSeq(t *testing.T) {
 				t.Error("put", err)
 			}
 		}
-		seq.CloseByProducer()
+		seq.Close()
 	}()
 
 	var c int
@@ -85,7 +85,7 @@ func Test_treeSeqSync(t *testing.T) {
 		}
 
 		wg.Wait()
-		seq.CloseByProducer()
+		seq.Close()
 	}()
 
 	c := 1
@@ -106,7 +106,7 @@ func Test_treeSeqSync(t *testing.T) {
 func Test_treeSeqClosed(t *testing.T) {
 	seq := newIntSeq()
 
-	seq.CloseByProducer()
+	seq.Close()
 	if err := seq.Put(128); err == nil {
 		t.Error("error expected")
 	}
@@ -121,13 +121,21 @@ func Test_treeSeqClosedAsyncClose(t *testing.T) {
 	req := require.New(t)
 	seq := NewSingleWriterSeq(10)
 
-	writerC := make(chan error, 1)
+	writerC := make(chan error, 2)
 	written := make(chan uint32, 1)
 
-	readerC := make(chan error, 1)
+	readerC := make(chan error, 2)
 	read := make(chan uint32, 1)
 
 	go func() {
+		defer func() {
+			if val := recover(); val != nil {
+				if err, ok := val.(error); ok {
+					writerC <- err
+				}
+			}
+		}()
+
 		current := uint32(1)
 		next := current
 		for {
@@ -143,15 +151,25 @@ func Test_treeSeqClosedAsyncClose(t *testing.T) {
 	}()
 
 	go func() {
+		defer func() {
+			if val := recover(); val != nil {
+				if err, ok := val.(error); ok {
+					readerC <- err
+				}
+			}
+		}()
+
 		current := uint32(0)
 		for {
 			val, err := seq.GetNextWithDeadline(time.Now().Add(time.Second))
 			if err != nil {
 				readerC <- err
 				read <- current
+				return
 			} else if val == nil {
 				readerC <- nil
 				read <- current
+				return
 			} else {
 				current = val.(uint32)
 			}
@@ -172,31 +190,237 @@ func Test_treeSeqClosedAsyncClose(t *testing.T) {
 	req.Equal(err, ErrClosed)
 	readCount := <-read
 	fmt.Printf("reader read %v\n", readCount)
-	req.Equal(writeCount, readCount)
+	req.Equal(int(writeCount), int(readCount))
+}
+
+func Test_treeSeqClosedMultiple(t *testing.T) {
+	req := require.New(t)
+	seq := NewSingleWriterSeq(10)
+
+	readerCount := 20
+
+	writerC := make(chan error, 5)
+	readerC := make(chan error, readerCount)
+
+	go func() {
+		defer func() {
+			if val := recover(); val != nil {
+				if err, ok := val.(error); ok {
+					writerC <- err
+				}
+			}
+		}()
+
+		current := uint32(1)
+		next := current
+		for {
+			if err := seq.PutSequenced(next, next); err != nil {
+				writerC <- err
+				return
+			}
+			current = next
+			next++
+			time.Sleep(time.Millisecond)
+		}
+	}()
+
+	waitGroup := &sync.WaitGroup{}
+	waitGroup.Add(readerCount)
+
+	for i := 0; i < readerCount; i++ {
+		go func() {
+			defer func() {
+				if val := recover(); val != nil {
+					if err, ok := val.(error); ok {
+						readerC <- err
+					}
+				}
+				waitGroup.Done()
+			}()
+
+			for {
+				val, err := seq.GetNextWithDeadline(time.Now().Add(time.Second))
+				if err != nil {
+					readerC <- err
+					return
+				} else if val == nil {
+					readerC <- nil
+					return
+				}
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+	seq.Close()
+	waitGroup.Wait()
+
+	done := false
+	for !done {
+		select {
+		case err := <-writerC:
+			req.Equal(err, ErrClosed)
+		default:
+			done = true
+		}
+	}
+
+	done = false
+	for !done {
+		select {
+		case err := <-readerC:
+			req.Equal(err, ErrClosed)
+		default:
+			done = true
+		}
+	}
+}
+
+func Test_treeSeqClosedByWriter(t *testing.T) {
+	req := require.New(t)
+	seq := NewSingleWriterSeq(10)
+
+	readerCount := 20
+
+	writerC := make(chan error, 5)
+	readerC := make(chan error, readerCount)
+
+	go func() {
+		defer func() {
+			if val := recover(); val != nil {
+				if err, ok := val.(error); ok {
+					writerC <- err
+				}
+			}
+		}()
+
+		start := time.Now()
+		current := uint32(1)
+		next := current
+		for {
+			if err := seq.PutSequenced(next, next); err != nil {
+				writerC <- err
+				return
+			}
+			current = next
+			next++
+			time.Sleep(time.Millisecond)
+
+			if current%1000 == 0 && time.Now().After(start.Add(time.Second)) {
+				seq.Close()
+				return
+			}
+		}
+	}()
+
+	waitGroup := &sync.WaitGroup{}
+	waitGroup.Add(readerCount)
+
+	for i := 0; i < readerCount; i++ {
+		go func() {
+			defer func() {
+				if val := recover(); val != nil {
+					if err, ok := val.(error); ok {
+						readerC <- err
+					}
+				}
+				waitGroup.Done()
+			}()
+
+			for {
+				val, err := seq.GetNextWithDeadline(time.Now().Add(time.Second))
+				if err != nil {
+					readerC <- err
+					return
+				} else if val == nil {
+					readerC <- nil
+					return
+				}
+			}
+		}()
+	}
+
+	waitGroup.Wait()
+
+	done := false
+	for !done {
+		select {
+		case err := <-writerC:
+			req.NoError(err)
+		default:
+			done = true
+		}
+	}
+
+	done = false
+	for !done {
+		select {
+		case err := <-readerC:
+			req.Equal(err, ErrClosed)
+		default:
+			done = true
+		}
+	}
 }
 
 func Test_treeSeqPreloaded(t *testing.T) {
-	seq := newIntSeq()
-
 	const BufferThreshold = 5000
+	seq := &singleWriterBtreeSeq{
+		maxOutOfOrder: int(BufferThreshold),
+		ch:            make(chan interface{}, BufferThreshold),
+		tree:          btree.NewWith(4, utils.UInt32Comparator),
+		nextSeq:       1,
+		closeNotify:   make(chan struct{}),
+	}
 
-	go func() {
-
-		r := rand.New(rand.NewSource(time.Now().Unix()))
-		for _, v := range r.Perm(BufferThreshold - 1) {
-			v = v + 1
-			if err := seq.Put(v); err != nil {
-				t.Error("put", err)
-			}
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, v := range r.Perm(BufferThreshold - 1) {
+		v = v + 1
+		if err := seq.PutSequenced(uint32(v), v); err != nil {
+			t.Error("put", err)
 		}
-		seq.CloseByProducer()
-
-	}()
+	}
+	seq.Close()
 
 	var c int
 	for c = 1; true; c++ {
 		v := seq.GetNext()
 		if v == nil {
+			break
+		}
+		if c != v.(int) {
+			t.Errorf("sequence is not in order, expected=%d, received=%d", c, v.(int))
+		}
+	}
+
+	if c != BufferThreshold {
+		t.Errorf("sequence did not complete correctly, expected=%d, received=%d", BufferThreshold, c)
+	}
+}
+
+func Test_treeSeqPreloadedDeadline(t *testing.T) {
+	const BufferThreshold = 5000
+	seq := &singleWriterBtreeSeq{
+		maxOutOfOrder: int(BufferThreshold),
+		ch:            make(chan interface{}, BufferThreshold),
+		tree:          btree.NewWith(4, utils.UInt32Comparator),
+		nextSeq:       1,
+		closeNotify:   make(chan struct{}),
+	}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, v := range r.Perm(BufferThreshold - 1) {
+		v = v + 1
+		if err := seq.PutSequenced(uint32(v), v); err != nil {
+			t.Error("put", err)
+		}
+	}
+	seq.Close()
+
+	var c int
+	for c = 1; true; c++ {
+		v, err := seq.GetNextWithDeadline(time.Now().Add(time.Millisecond))
+		if err == ErrClosed {
 			break
 		}
 		if c != v.(int) {
@@ -216,7 +440,7 @@ func newMultiWriterBtreeSeq(seqF func(interface{}) uint32) *multiWriterBtreeSeq 
 			tree:          btree.NewWith(4, utils.UInt32Comparator),
 			nextSeq:       1,
 			maxOutOfOrder: math.MaxUint32,
-			ticker:        time.NewTicker(time.Second),
+			closeNotify:   make(chan struct{}),
 		},
 		writeCh: make(chan *multiWriterSeqEntry),
 	}
