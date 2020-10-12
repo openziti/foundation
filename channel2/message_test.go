500fc5b8d6021b47d70cf627fb204a566617974f
Eugene K <eugene.kobyakov@netfoundry.io>
2020-10-12 14:18:26 -0400

    use more compact integer headers

--- channel2/message_test.go
+++ channel2/message_test.go
@@ -61,3 +61,126 @@ func Test_getRetryVersionFor(t *testing.T) {
 		})
 	}
 }
+
+func TestMessageHeader_PutUint64Header(t *testing.T) {
+	testHeader := int32(1111)
+	type args struct {
+		key   int32
+		value uint64
+	}
+	tests := []struct {
+		name   string
+		len    int
+		args   args
+	}{
+		{name: "0 byte", len: 0, args: args{ key:testHeader, value: uint64(0)}},
+		{name: "1 byte", len: 1, args: args{ key:testHeader, value: uint64(0x1)}},
+		{name: "2 byte", len: 2, args: args{ key:testHeader, value: uint64(0x0201)}},
+		{name: "3 byte", len: 3, args: args{ key:testHeader, value: uint64(0x030201)}},
+		{name: "4 byte", len: 4, args: args{ key:testHeader, value: uint64(0x04030201)}},
+		{name: "5 byte", len: 5, args: args{ key:testHeader, value: uint64(0x0504030201)}},
+		{name: "6 byte", len: 6, args: args{ key:testHeader, value: uint64(0x060504030201)}},
+		{name: "7 byte", len: 7, args: args{ key:testHeader, value: uint64(0x07060504000001)}},
+		{name: "8 byte", len: 8, args: args{ key:testHeader, value: uint64(0x0807060504000000)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			header := &MessageHeader{
+				Headers:     make(map[int32][]byte),
+			}
+
+			header.PutUint64Header(tt.args.key, tt.args.value)
+			binary, err := marshalHeaders(header.Headers)
+			if err != nil {
+				t.Error(tt.name, err)
+			}
+
+			header.Headers, err = unmarshalHeaders(binary)
+			if err != nil {
+				t.Error(tt.name, err)
+			}
+
+			if tt.len != len(header.Headers[testHeader]) {
+				t.Errorf("%s: unexpected header len", tt.name)
+			}
+
+			v, found := header.GetUint64Header(tt.args.key)
+			if !found {
+				t.Errorf("%s: put failed", tt.name)
+			}
+			if v != tt.args.value {
+				t.Errorf("%s: %d(%v) != %v", tt.name, v, header.Headers[tt.args.key], tt.args.value)
+			}
+		})
+	}
+}
+
+func TestMessageHeader_PutUint32Header(t *testing.T) {
+	testHeader := int32(1111)
+	type args struct {
+		key   int32
+		value uint32
+	}
+	tests := []struct {
+		name   string
+		len    int
+		args   args
+	}{
+		{name: "0 byte", len:0, args: args{ key:testHeader, value: uint32(0)}},
+		{name: "1 byte", len:1, args: args{ key:testHeader, value: uint32(0x1)}},
+		{name: "2 byte", len:2, args: args{ key:testHeader, value: uint32(0x0201)}},
+		{name: "3 byte", len:3, args: args{ key:testHeader, value: uint32(0x030201)}},
+		{name: "4 byte", len:4, args: args{ key:testHeader, value: uint32(0x04030201)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			header := &MessageHeader{
+				Headers:     make(map[int32][]byte),
+			}
+
+			header.PutUint32Header(tt.args.key, tt.args.value)
+			if tt.len != len(header.Headers[testHeader]) {
+				t.Errorf("%s: unexpected header len", tt.name)
+			}
+			v, found := header.GetUint32Header(tt.args.key)
+			if !found {
+				t.Errorf("%s: put failed", tt.name)
+			}
+			if v != tt.args.value {
+				t.Errorf("%s: %d(%v) != %v", tt.name, v, header.Headers[tt.args.key], tt.args.value)
+			}
+		})
+	}
+}
+
+func TestMessageHeader_PutUint16Header(t *testing.T) {
+	testHeader := int32(1111)
+	type args struct {
+		key   int32
+		value uint16
+	}
+	tests := []struct {
+		name   string
+		args   args
+	}{
+		{name: "0 byte", args: args{ key:testHeader, value: uint16(0)}},
+		{name: "1 byte", args: args{ key:testHeader, value: uint16(0x1)}},
+		{name: "2 byte", args: args{ key:testHeader, value: uint16(0xFF00)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			header := &MessageHeader{
+				Headers:     make(map[int32][]byte),
+			}
+
+			header.PutUint16Header(tt.args.key, tt.args.value)
+			v, found := header.GetUint16Header(tt.args.key)
+			if !found {
+				t.Errorf("%s: put failed", tt.name)
+			}
+			if v != tt.args.value {
+				t.Errorf("%s: %d(%v) != %v", tt.name, v, header.Headers[tt.args.key], tt.args.value)
+			}
+		})
+	}
+}
