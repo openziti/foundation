540fdf7d0ce35429bc21d8abec35f19f6a144a85
Paul Lorenz <paul.lorenz@netfoundry.io>
2020-11-06 15:05:57 -0500

    Add send with buffer copy method to channel

--- channel2/noop_test_impl.go
+++ channel2/noop_test_impl.go
@@ -29,35 +29,35 @@ func (ch *NoopTestChannel) Label() string {
 	return "testchannel"
 }
 
-func (ch *NoopTestChannel) SetLogicalName(logicalName string) {
+func (ch *NoopTestChannel) SetLogicalName(string) {
 	panic("implement SetLogicalName")
 }
 
-func (ch *NoopTestChannel) Bind(h BindHandler) error {
+func (ch *NoopTestChannel) Bind(BindHandler) error {
 	panic("implement Bind")
 }
 
-func (ch *NoopTestChannel) AddPeekHandler(h PeekHandler) {
+func (ch *NoopTestChannel) AddPeekHandler(PeekHandler) {
 	panic("implement AddPeekHandler")
 }
 
-func (ch *NoopTestChannel) AddTransformHandler(h TransformHandler) {
+func (ch *NoopTestChannel) AddTransformHandler(TransformHandler) {
 	panic("implement AddTransformHandler")
 }
 
-func (ch *NoopTestChannel) AddReceiveHandler(h ReceiveHandler) {
+func (ch *NoopTestChannel) AddReceiveHandler(ReceiveHandler) {
 	panic("implement AddReceiveHandler")
 }
 
-func (ch *NoopTestChannel) AddErrorHandler(h ErrorHandler) {
+func (ch *NoopTestChannel) AddErrorHandler(ErrorHandler) {
 	panic("implement me")
 }
 
-func (ch *NoopTestChannel) AddCloseHandler(h CloseHandler) {
+func (ch *NoopTestChannel) AddCloseHandler(CloseHandler) {
 	panic("implement AddErrorHandler")
 }
 
-func (ch *NoopTestChannel) SetUserData(data interface{}) {
+func (ch *NoopTestChannel) SetUserData(interface{}) {
 	panic("implement SetUserData")
 }
 
@@ -65,11 +65,15 @@ func (ch *NoopTestChannel) GetUserData() interface{} {
 	panic("implement GetUserData")
 }
 
-func (ch *NoopTestChannel) Send(m *Message) error {
+func (ch *NoopTestChannel) Send(*Message) error {
 	return nil
 }
 
-func (ch *NoopTestChannel) SendWithPriority(m *Message, p Priority) error {
+func (ch *NoopTestChannel) SendWithPriority(*Message, Priority) error {
+	return nil
+}
+
+func (ch *NoopTestChannel) SendBufferSafeWithPriority(*Message, Priority) error {
 	return nil
 }
 
@@ -77,41 +81,41 @@ func (ch *NoopTestChannel) SendAndSync(m *Message) (chan error, error) {
 	return ch.SendAndSyncWithPriority(m, Standard)
 }
 
-func (ch *NoopTestChannel) SendAndSyncWithPriority(m *Message, p Priority) (chan error, error) {
+func (ch *NoopTestChannel) SendAndSyncWithPriority(*Message, Priority) (chan error, error) {
 	result := make(chan error, 1)
 	result <- nil
 	return result, nil
 }
 
-func (ch *NoopTestChannel) SendWithTimeout(m *Message, timeout time.Duration) error {
+func (ch *NoopTestChannel) SendWithTimeout(*Message, time.Duration) error {
 	return nil
 }
 
-func (ch *NoopTestChannel) SendPrioritizedWithTimeout(m *Message, p Priority, timeout time.Duration) error {
+func (ch *NoopTestChannel) SendPrioritizedWithTimeout(*Message, Priority, time.Duration) error {
 	return nil
 }
 
-func (ch *NoopTestChannel) SendAndWaitWithTimeout(m *Message, timeout time.Duration) (*Message, error) {
+func (ch *NoopTestChannel) SendAndWaitWithTimeout(*Message, time.Duration) (*Message, error) {
 	panic("implement SendAndWaitWithTimeout")
 }
 
-func (ch *NoopTestChannel) SendPrioritizedAndWaitWithTimeout(m *Message, p Priority, timeout time.Duration) (*Message, error) {
+func (ch *NoopTestChannel) SendPrioritizedAndWaitWithTimeout(*Message, Priority, time.Duration) (*Message, error) {
 	panic("implement SendPrioritizedAndWaitWithTimeout")
 }
 
-func (ch *NoopTestChannel) SendAndWait(m *Message) (chan *Message, error) {
+func (ch *NoopTestChannel) SendAndWait(*Message) (chan *Message, error) {
 	panic("implement SendAndWait")
 }
 
-func (ch *NoopTestChannel) SendAndWaitWithPriority(m *Message, p Priority) (chan *Message, error) {
+func (ch *NoopTestChannel) SendAndWaitWithPriority(*Message, Priority) (chan *Message, error) {
 	panic("implement SendAndWaitWithPriority")
 }
 
-func (ch *NoopTestChannel) SendForReply(msg TypedMessage, timeout time.Duration) (*Message, error) {
+func (ch *NoopTestChannel) SendForReply(TypedMessage, time.Duration) (*Message, error) {
 	panic("implement SendForReply")
 }
 
-func (ch *NoopTestChannel) SendForReplyAndDecode(msg TypedMessage, timeout time.Duration, result TypedMessage) error {
+func (ch *NoopTestChannel) SendForReplyAndDecode(TypedMessage, time.Duration, TypedMessage) error {
 	return nil
 }
 
