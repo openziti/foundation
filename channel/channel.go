af03002665a39c0eacddcaa0fc6cc27269be28fb
Paul Lorenz <paul.lorenz@netfoundry.io>
2022-01-17 14:46:35 -0500

    Refactor channel for better send ergonomics

---
+++ channel/channel.go
@@ -0,0 +1,125 @@
+/*
+	Copyright NetFoundry, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package channel
+
+import (
+	"context"
+	"crypto/x509"
+	"github.com/openziti/foundation/identity/identity"
+	"github.com/openziti/foundation/transport"
+	"io"
+	"time"
+)
+
+// Channel represents an asyncronous, message-passing framework, designed to sit on top of an underlay.
+//
+type Channel interface {
+	Identity
+	SetLogicalName(logicalName string)
+	Binding
+	Sender
+	io.Closer
+	IsClosed() bool
+	Underlay() Underlay
+	StartRx()
+	GetTimeSinceLastRead() time.Duration
+}
+
+type SendContext interface {
+	Msg() *Message
+	Priority() Priority
+	WithPriority(p Priority) SendContext
+	Context() context.Context
+	WithTimeout(duration time.Duration) TimeoutSendContext
+	Sequence() int32
+	NotifyBeforeWrite()
+	NotifyAfterWrite()
+	NotifyErr(err error)
+	ReplyChan() chan<- *Message
+}
+
+type TimeoutSendContext interface {
+	SendContext
+	SendAndWaitForWire(ch Channel) error
+	SendForReply(ch Channel) (*Message, error)
+	SendForTypedReply(ch Channel, result TypedMessage) error
+}
+
+type Identity interface {
+	// The TokenId used to represent the identity of this channel to lower-level resources.
+	//
+	Id() *identity.TokenId
+
+	// The LogicalName represents the purpose or usage of this channel (i.e. 'ctrl', 'mgmt' 'r/001', etc.) Usually used
+	// by humans in understand the logical purpose of a channel.
+	//
+	LogicalName() string
+
+	// The ConnectionId represents the identity of this Channel to internal API components ("instance identifier").
+	// Usually used by the Channel framework to differentiate Channel instances.
+	//
+	ConnectionId() string
+
+	// Certificates contains the identity certificates provided by the peer.
+	//
+	Certificates() []*x509.Certificate
+
+	// Label constructs a consistently-formatted string used for context logging purposes, from the components above.
+	//
+	Label() string
+}
+
+// UnderlayListener represents a component designed to listen for incoming peer connections.
+//
+type UnderlayListener interface {
+	Listen(handlers ...ConnectionHandler) error
+	UnderlayFactory
+	io.Closer
+}
+
+// UnderlayFactory is used by Channel to obtain an Underlay instance. An underlay "dialer" or "listener" implement
+// UnderlayFactory, to provide instances to Channel.
+//
+type UnderlayFactory interface {
+	Create(timeout time.Duration, tcfg transport.Configuration) (Underlay, error)
+}
+
+// Underlay abstracts a physical communications channel, typically sitting on top of 'transport'.
+//
+type Underlay interface {
+	Rx() (*Message, error)
+	Tx(m *Message) error
+	Identity
+	io.Closer
+	IsClosed() bool
+	Headers() map[int32][]byte
+}
+
+type Sender interface {
+	Send(sendCtx SendContext) error
+}
+
+const AnyContentType = -1
+const HelloSequence = -1
+
+var ListenerClosedError = listenerClosedError{}
+
+type listenerClosedError struct{}
+
+func (err listenerClosedError) Error() string {
+	return "closed"
+}
---
+++ channel/cmd/channel/subcmd/memory/run.go
@@ -0,0 +1,134 @@
+/*
+	Copyright NetFoundry, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package underlay
+
+import (
+	"github.com/michaelquigley/pfxlog"
+	"github.com/openziti/foundation/channel2"
+	"github.com/openziti/foundation/channel2/cmd/channel2/subcmd"
+	"github.com/openziti/foundation/identity/dotziti"
+	"github.com/openziti/foundation/identity/identity"
+	"github.com/spf13/cobra"
+	"time"
+)
+
+func init() {
+	memory.Flags().StringVarP(&memoryIdentity, "identity", "i", "default", ".ziti Identity")
+	memory.Flags().IntVarP(&memoryCount, "count", "c", 100, "number of messages to send")
+	subcmd.Root.AddCommand(memory)
+}
+
+var memory = &cobra.Command{
+	Use:   "memory",
+	Short: "colocated dialer/listener using memory underlay",
+	Run:   runMemory,
+}
+var memoryIdentity string
+var memoryCount int
+
+var listenerDone = make(chan struct{})
+var dialerDone = make(chan struct{})
+
+func runMemory(_ *cobra.Command, _ []string) {
+	_, id, err := dotziti.LoadIdentity(memoryIdentity)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := channel2.NewMemoryContext()
+	go handleDialer(id, ctx)
+	go runListener(id, ctx)
+
+	<-listenerDone
+	<-dialerDone
+
+	/*
+	 * Time for the rxer/txer goroutines to exit.
+	 */
+	time.Sleep(1 * time.Second)
+	/* */
+}
+
+func handleDialer(identity *identity.TokenId, ctx *channel2.MemoryContext) {
+	log := pfxlog.Logger()
+
+	options := channel2.DefaultOptions()
+	options.BindHandlers = []channel2.BindHandler{&bindHandler{}}
+
+	dialer := channel2.NewMemoryDialer(identity, nil, ctx)
+	ch, err := channel2.NewChannel("memory", dialer, options)
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("channel label = [%s]", ch.Label())
+
+	log = pfxlog.ContextLogger(ch.Label())
+	for i := 0; i < memoryCount; i++ {
+		if err := ch.Send(newMessage(i)); err != nil {
+			log.Errorf("error sending (%s)", err)
+			break
+		}
+		log.Infof("send = [%d]", i)
+	}
+
+	if err := ch.Close(); err != nil {
+		panic(err)
+	}
+
+	close(dialerDone)
+}
+
+func runListener(identity *identity.TokenId, ctx *channel2.MemoryContext) {
+	log := pfxlog.Logger()
+
+	listener := channel2.NewMemoryListener(identity, ctx)
+	if err := listener.Listen(); err != nil {
+		panic(err)
+	}
+
+	options := channel2.DefaultOptions()
+	options.BindHandlers = []channel2.BindHandler{&bindHandler{}}
+
+	ch, err := channel2.NewChannel("memory", listener, options)
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("channel label = [%s]", ch.Label())
+
+	go handleListener(ch)
+
+	if err := listener.Close(); err != nil {
+		log.Errorf("error closing listener (%s)", err)
+	}
+}
+
+func handleListener(ch channel2.Channel) {
+	log := pfxlog.ContextLogger(ch.Label())
+	for i := 0; i < memoryCount; i++ {
+		if err := ch.Send(newMessage(i)); err != nil {
+			log.Errorf("error sending (%s)", err)
+			break
+		}
+		log.Infof("send = [%d]", i)
+	}
+
+	if err := ch.Close(); err != nil {
+		panic(err)
+	}
+
+	close(listenerDone)
+}
