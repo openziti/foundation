f94b311642ea82909f0c377165eec016f8e934b8
Paul Lorenz <paul.lorenz@netfoundry.io>
2022-05-03 14:41:06 -0400

    Add copyright headers

--- util/concurrenz/copy_on_write_map.go
+++ util/concurrenz/copy_on_write_map.go
@@ -1,3 +1,19 @@
+/*
+	Copyright NetFoundry, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 package concurrenz
 
 import "sync"
@@ -26,4 +42,10 @@ func (self *CopyOnWriteMap[K, V]) Get(key K) V {
 
 func (self *CopyOnWriteMap[K, V]) AsMap() map[K]V {
 	return self.value.Load()
+}
+
+func (self *CopyOnWriteMap[K, V]) Clear() {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	self.value.Store(map[K]V{})
 }
