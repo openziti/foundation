625a992ee73cd4f80d042e2d3b17324d0970a360
Paul Lorenz <paul.lorenz@netfoundry.io>
2025-05-02 10:06:50 -0400

    Update golangci to v2

--- stringz/utils.go
+++ stringz/utils.go
@@ -16,6 +16,8 @@
 
 package stringz
 
+import "fmt"
+
 func EqualSlices(a []string, b []string) bool {
 	if len(a) != len(b) {
 		return false
@@ -129,4 +131,21 @@ func SliceToSet(values []string) map[string]struct{} {
 		result[val] = struct{}{}
 	}
 	return result
+}
+
+var units = []string{"B", "K", "M", "G", "T", "P"}
+
+func FormatBytes(val uint64) string {
+	var i int
+	var target uint64
+	for i = range units {
+		target = 1 << uint(10*(i+1))
+		if val < target {
+			break
+		}
+	}
+	if i > 0 {
+		return fmt.Sprintf("%0.2f%s", float64(val)/(float64(target)/1024), units[i])
+	}
+	return fmt.Sprintf("%dB", val)
 }
